25d5dffaa6e040316c0a2295119cf85df18f73ab
Triantara-TI <72885952+Triantara-TI@users.noreply.github.com>
2020-11-01 14:27:57 +0800

    Add files via upload

---
+++ app/Http/Controllers/MeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class MeController extends Controller
+
+{
+    public function satu()
+    {
+        return view("view-4");
+    }
+
+    public function dua()
+    {
+        return view("view-5");
+    }
+
+    public function tiga()
+    {
+        return view("view-6");
+    }
+}
