e94c4248c653d9958353602e3f5e5128911c32e7
Triantara-TI <72885952+Triantara-TI@users.noreply.github.com>
2020-11-01 15:34:59 +0800

    Add files via upload

---
+++ views/view-3.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Triantara Pujo Semedi</title>
+</head>
+<body>
+    <h1>TEKNIK informatika</h1>
+    <p>nama saya Pujo</p>
+    <p>view 3</p>
+</body>
+</html>
---
+++ views/view-5.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Triantara Pujo S</title>
+</head>
+<body>
+    <h1>TEKNIK informatika</h1>
+    <p>nama saya Pujo</p>
+    <p>view 5</p>
+</body>
+</html>
